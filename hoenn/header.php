0f89cbcac8ad9ce72ea376a6b204ab7f51260a03
Alfredo Barrón <alfreedobarron@gmail.com>
2014-10-07 11:14:53 -0500

    first commit

---
+++ hoenn/header.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Centros Pok&eacute;mon</title>
+    	<link rel="shortcut icon" href="images/pokebola.gif">
+    	<!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="./dist/css/bootstrap.min.css">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="./dist/css/bootstrap-theme.min.css">
+    </head>
+	<body>
---
+++ hoenn/savetipo.php
@@ -0,0 +1,18 @@
+<?php
+include "DB.php";
+$nombre = $_POST['nombre'];
+$id = (isset($_POST['id'])) ? $_POST['id'] : 0;
+
+if($nombre != ""){
+	$db = new DB("root","root","localhost","centrospokemon");
+    $data = array();
+    if($id == 0){
+    	$data = array('nombre' => $nombre);
+    }
+    else {
+   		$data = array('nombre' => $nombre, 'id' => $id);
+  }
+  }
+  $db->save("catalogo_tipos",$data);
+header("Location:catalogo_tipos.php");
+?>
