f1f9ddedf2d5d80b712dd00d540a1d95220db234
Alfredo Barrón Rodríguez <alfreedobarron@gmail.com>
2014-12-02 19:33:44 -0600

    hola

--- app/controllers/routes.controller.php
+++ app/controllers/routes.controller.php
@@ -78,26 +78,8 @@ $app->get('/pokemon(/:id)', function($id = null) use($app){
   if($id == null){
     $pokemon = Pokemon::all();
     echo $pokemon->toJson();
-    /*$i = 0;
-    foreach ($pokemon as $p) {
-      $data[$i] =  array(
-          'id' => $p->id,
-          'species' => $p->species,
-          'image' => $p->image,
-          'region_id' => $p->region_id,
-          'hit_points' => $p->hit_points,
-          'attack' => $p->attack,
-          'defense' => $p->defense,
-          'speed' => $p->speed,
-          'evasion' => $p->evasion,
-          'accuracy' => $p->accuracy,
-          );pokemon
-      $i++;
-    }
-    $response['pokemon'] = $data;
-    echo json_encode($response); */
   }else{
-    $pokemon = Pokemon::where('id',$id)->first();
+    $pokemon = Pokemon:::with('types')->where('id',$id)->first();
     echo $pokemon->toJson();
   }
 });
